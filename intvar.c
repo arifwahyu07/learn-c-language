8fdb3504dc8c9e7088d6c62db6bed75392c57e18
arifwahyu07 <arif.ws8@gmail.com>
2020-12-23 23:24:53 +0700

    belajar memahami nama variabel dan bagaimana memperkenalkannya ke program belajar memahami makna dan dampak konstruksi " = " dalam bahasa C dalam deklarasi variabel belajar memahami dampak eksekusi " %d " dan hubungannya dengan nilai variabel, pada perintah printf dalam bahasa C

---
+++ intvar.c
@@ -0,0 +1,11 @@
+/* File intvar.c */
+/* Penulis : Arif wahyu, email arif.ws8@gmail.com */
+/* Mengisi Variabel i yg bertype int dengan nilai 5 dan menuliskannya */
+#include<studio.h>
+int main ()
+{/* Kamus */
+    int i = 5; /* deklarasi dan inisialisasi nilai variabel i dengan 5 */
+    /* Algoritma */
+    printf ("Ini nilai i : %d \n", i);/*untuk mengamati dampak inisialisasi*/
+    return 0;
+}
