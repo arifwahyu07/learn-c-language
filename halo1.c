c68806097f817f22204789888345ef12661c453e
arifwahyu07 <arif.ws8@gmail.com>
2020-12-23 20:38:18 +0700

    belajar membedakan makna void main dengan void int main. belajar memahami konsep return dan manfaatnya walaupun belum memanfaatkannya

---
+++ halo1.c
@@ -0,0 +1,13 @@
+/* File : halo1.c */
+/* Penulis : Arif Wahyu, email arif.ws8@gmail.com */
+/* Program untuk menuliskan Halo Dunia! ke layar */
+#include<studio.h>
+int
+main ()
+{
+    /* KAMUS */
+
+    /* ALGORITMA */
+    printf ("Halo Dunia\n");
+    return 0;
+}
