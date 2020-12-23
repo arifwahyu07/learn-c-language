844d2de9d59aae5735aebe170c43995e234df202
arifwahyu07 <arif.ws8@gmail.com>
2020-12-23 19:50:17 +0700

    belajar mengenal kerangka program utama

---
+++ halo.c
@@ -0,0 +1,9 @@
+/* File : halo.c */
+/* Penulis : Bu Dengklek, email inge@brebes.or.id */
+/* Program menuliskan "Halo Dunia" dan "end-of-line" (ganti baris) */
+#include<studio.h>
+int
+main(void)
+{ /* Badan Program */
+printf("Halo Dunia!\n ");
+}
